9428148e6b1ec49a007d055c3faf26bb829a5053
Luk-kar <59205324+Lukkar90@users.noreply.github.com>
2021-04-29 18:03:02 +0200

    update

--- __main__.py
+++ __main__.py
@@ -9,15 +9,15 @@ if len(_argv) == 2:
 
     valid_extensions = (".png", ".jpg", ".jpeg", ".bmp")
     paths_files_source = []
+    target_path = _argv[1]
+    similar_images = {}
 
-    for path, directories, files in os.walk(_argv[1]):
+    for path, directories, files in os.walk(target_path):
         for filename in files:
             if filename.endswith(valid_extensions):
-                paths_files_source.append(f"{_argv[1]}\{filename}")
+                paths_files_source.append(f"{target_path}\{filename}")
 
     paths_files_target = paths_files_source.copy()
-
-    similar_images = {}
 
     for source_path_file in paths_files_source:
         similar_images[source_path_file] = []
@@ -27,9 +27,26 @@ if len(_argv) == 2:
             ssim = compare_images(source_path_file, target_path_file)
             if ssim > 0.8 and target_path_file != source_path_file:
                 similar_images[source_path_file].append(target_path_file)
-                # paths_files_target.remove(target_path_file)
 
-    print(similar_images)
+    # for image in similar_images:
+    dir_output = f"{target_path}\\similar images"
+    if not os.path.isdir(dir_output):
+        os.mkdir(dir_output)
+
+    for i in similar_images:
+        if similar_images[i] != []:
+            filename = os.path.basename(i)
+            dir_name = os.path.splitext(filename)[0]
+            path_to_shortcuts = f"{dir_output}\\{dir_name}"
+
+            if not os.path.isdir(path_to_shortcuts):
+                os.mkdir(path_to_shortcuts)
+
+            for image in similar_images[i]:
+                image_file = os.path.basename(image)
+                image_name = os.path.splitext(image_file)[0]
+                path = os.path.join(path_to_shortcuts, f'{image_name}.lnk')
+                print(path)
 
 else:
     print("wrong input")
