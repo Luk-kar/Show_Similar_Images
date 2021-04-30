b2fdefcc0d4a64dce872260310cd84258a4119e7
Luk-kar <59205324+Lukkar90@users.noreply.github.com>
2021-04-30 12:52:58 +0200

    update

--- compare_two_images.py
+++ compare_two_images.py
@@ -1,18 +1,16 @@
 # import the necessary packages
-from skimage.metrics import structural_similarity as ssim
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2
+from image_match.goldberg import ImageSignature
 
 
 def compare_images(path_image_A, path_image_B):
 
-    imageA = cv2.imread(path_image_A)
-    imageB = cv2.imread(path_image_B)
+    gis = ImageSignature()
 
-    imageA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-    imageB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+    a = gis.generate_signature(path_image_A)
+    b = gis.generate_signature(path_image_B)
+    diff_value = 1 - gis.normalized_distance(a, b)
 
     # compute the mean squared error and structural similarity
     # index for the images
-    return ssim(imageA, imageB)
+
+    return diff_value
