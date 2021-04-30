d12bd3c7199e5f641264e7e9f5677394f417236a
Luk-kar <59205324+Lukkar90@users.noreply.github.com>
2021-04-30 16:34:26 +0200

    update

--- UI/MainGUIApp.py
+++ UI/MainGUIApp.py
@@ -9,34 +9,43 @@ class Demo1:
 
         self.frame = tk.Frame(self.master, padx=10, pady=15)
 
-        self.title = tk.Label(
-            self.frame, text="Target images")
-        self.title.grid(column=0, row=0)
+        self.target_path_title = tk.Label(self.frame, text="Target images")
+        self.target_path_title.grid(column=0, row=0)
 
         self.target_path_entry = tk.Entry(self.frame)
         self.target_path_entry.grid(column=0, row=1)
 
+        self.extensions = [".png", ".jpg/.jpeg", ".bmp"]
+        self.checkbars = Checkbar(self.frame, self.extensions)
+        self.checkbars.grid(column=0, row=2)
+
+        self.similarity_title = tk.Label(self.frame, text="Similarity")
+        self.similarity_title.grid(column=0, row=3)
+
+        self.similarity_entry = tk.Entry(self.frame)
+        self.similarity_entry.grid(column=0, row=4)
+
         self.button1 = tk.Button(
-            self.frame, text='New Window', width=25, command=self.new_window)
-        self.button1.grid(column=0, row=2)
+            self.frame, text='Find similar images', width=25, command=self.new_window)
+        self.button1.grid(column=0, row=5)
         self.frame.grid()
 
     def new_window(self):
-        self.newWindow = tk.Toplevel(self.master)
-        self.app = Demo2(self.newWindow)
+        print(list(self.checkbars.state()))
 
 
-class Demo2:
-    def __init__(self, master):
-        self.master = master
-        self.frame = tk.Frame(self.master)
-        self.quitButton = tk.Button(
-            self.frame, text='Quit', width=25, command=self.close_windows)
-        self.quitButton.pack()
-        self.frame.pack()
+class Checkbar(tk.Frame):
+    def __init__(self, parent=None, picks=[], side=tk.LEFT, anchor=tk.W):
+        tk.Frame.__init__(self, parent)
+        self.vars = []
+        for pick in picks:
+            var = tk.IntVar()
+            chk = tk.Checkbutton(self, text=pick, variable=var)
+            chk.pack(side=side, anchor=anchor, expand=tk.YES)
+            self.vars.append(var)
 
-    def close_windows(self):
-        self.master.destroy()
+    def state(self):
+        return map((lambda var: var.get()), self.vars)
 
 
 def MainGUIApp():
