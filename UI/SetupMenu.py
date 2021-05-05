79e2190c00a472ea86367f06b9613b599e57cdda
Luk-kar <59205324+Lukkar90@users.noreply.github.com>
2021-05-05 11:58:02 +0200

    update

--- UI/SetupMenu.py
+++ UI/SetupMenu.py
@@ -5,103 +5,18 @@ from configparser import ConfigParser
 from tkinter import filedialog  # for Python 3
 from tkinter import messagebox
 
-from config import set_app_path
-
 from UI.helpers.open_folder import open_folder
-
-
-def get_DEFAULT_ini_path():
-    return os.path.join(set_app_path(), "appData")
-
-
-def get_save_file_ini_path():
-    return filedialog.asksaveasfilename(initialdir=get_DEFAULT_ini_path(), title="Save setup file", filetypes=[("Setup files", "*.ini")])
-
-
-def get_open_file_ini_path():
-    return filedialog.askopenfilename(initialdir=get_DEFAULT_ini_path(), title="Open setup file", filetypes=[("Setup files", "*.ini")])
-
-
-def setup_saving_to_ini(
-        setup_path,
-        checkedboxes,
-        target_path,
-        similarity
-):
-
-    config = ConfigParser()
-
-    config["MATCHING"] = {
-        "images path": target_path,
-    }
-
-    config["FILE TYPES"] = {
-        ".png": checkedboxes[0],
-        ".jpg/.jpeg": checkedboxes[1],
-        ".bmp": checkedboxes[2]
-    }
-
-    config["MINIMAL SIMILARITY"] = {
-        "value": similarity,
-    }
-
-    if setup_path:
-        with open(setup_path, "w") as configfile:
-            config.write(configfile)
-    else:
-        raise OSError("There is no save path")
-
-
-def read_config_file(file):
-    """return string"""
-
-    config = ConfigParser()
-
-    try:
-        with open(file) as f:
-            config.read_file(f)
-    except IOError as error:
-        raise IOError(error)
-
-    return config
-
-
-DEFAULTS_file_path = "_DEFAULT.ini"
-
-
-def get_images_folder_path(config):
-    return config.get("MATCHING", "images path")
-
-
-def get_similarity(config):
-    return config.get("MINIMAL SIMILARITY", "value")
-
-
-def get_checked_extensions(config):
-    return config.items("FILE TYPES")
-
-
-def create_DEFAULT_setup_file(setup_path):
-    valid_extensions = [[".png", 1], [".jpg/.jpeg", 0], [".bmp", 0]]
-    checkedboxes = list(map(lambda x: x[1], valid_extensions))
-    target_path = ""
-    similarity = 0.8
-
-    setup_saving_to_ini(
-        setup_path,
-        checkedboxes,
-        target_path,
-        similarity
-    )
-
+import config as setup
 
 # https://stackoverflow.com/questions/31170616/how-to-access-a-method-in-one-inherited-tkinter-class-from-another-inherited-tki
+
+
 class SetupMenu(tk.Menu):
     def __init__(self, parent, main):
         tk.Menu.__init__(self, parent)
 
         self.main = main
-        self.ini_default_location = get_DEFAULT_ini_path()
+        self.ini_default_location = setup.get_DEFAULT_ini_path()
 
         setupMenu = tk.Menu(self, tearoff=False)
         self.add_cascade(label="Setup", underline=0, menu=setupMenu)
@@ -124,14 +39,14 @@ class SetupMenu(tk.Menu):
 
         main = self.main
 
-        setup_path = get_save_file_ini_path()
+        setup_path = setup.get_save_file_ini_path()
 
         if setup_path:
             checkedboxes = list(main.checkbars.state())
             target_path = main.target_path_entry.get()
             similarity = float(main.similarity_entry.get())
 
-            setup_saving_to_ini(
+            setup.setup_saving_to_ini(
                 setup_path,
                 checkedboxes,
                 target_path,
@@ -144,7 +59,6 @@ class SetupMenu(tk.Menu):
             )
 
             setup_folder = os.path.dirname(setup_path)
-            open_folder(setup_folder)
 
         else:
 
@@ -155,10 +69,10 @@ class SetupMenu(tk.Menu):
 
     def setup_open(self):
 
-        setup_path = get_open_file_ini_path()
+        setup_path = setup.get_open_file_ini_path()
 
         if setup_path:
-            config = read_config_file(setup_path)
+            config = setup.read_config_file(setup_path)
             self.dialogs_set_setup(config)
         else:
             messagebox.showinfo(
@@ -171,12 +85,12 @@ class SetupMenu(tk.Menu):
         main = self.main
 
         setup_path = os.path.join(
-            self.ini_default_location, DEFAULTS_file_path)
+            self.ini_default_location, setup.DEFAULTS_file_path)
         checkedboxes = list(main.checkbars.state())
         target_path = main.target_path_entry.get()
         similarity = float(main.similarity_entry.get())
 
-        setup_saving_to_ini(
+        setup.setup_saving_to_ini(
             setup_path,
             checkedboxes,
             target_path,
@@ -185,22 +99,22 @@ class SetupMenu(tk.Menu):
 
     def setup_reset_to_defaults(self):
         setup_path = os.path.join(
-            self.ini_default_location, DEFAULTS_file_path)
+            self.ini_default_location, setup.DEFAULTS_file_path)
 
         if not os.path.exists(setup_path):
 
-            create_DEFAULT_setup_file(setup_path)
+            setup.create_DEFAULT_setup_file(setup_path)
 
-        config = read_config_file(setup_path)
+        config = setup.read_config_file(setup_path)
         self.dialogs_set_setup(config)
 
     def setup_default_reset(self):
         setup_path = os.path.join(
-            self.ini_default_location, DEFAULTS_file_path)
+            self.ini_default_location, setup.DEFAULTS_file_path)
 
-        create_DEFAULT_setup_file(setup_path)
+        setup.create_DEFAULT_setup_file(setup_path)
 
-        config = read_config_file(setup_path)
+        config = setup.read_config_file(setup_path)
         self.dialogs_set_setup(config)
 
     def dialogs_set_setup(self, config):
@@ -208,12 +122,12 @@ class SetupMenu(tk.Menu):
         main = self.main
 
         main.target_path_entry = main.entry_set(
-            main.target_path_entry, get_images_folder_path(config)
+            main.target_path_entry, setup.get_images_folder_path(config)
         )
 
-        picks = get_checked_extensions(config)
+        picks = setup.get_checked_extensions(config)
         main.checkbars.set_state(picks)
 
         main.similarity_entry = main.entry_set(
-            main.similarity_entry, get_similarity(config)
+            main.similarity_entry, setup.get_similarity(config)
         )
--- config.py
+++ config.py
@@ -1,7 +1,95 @@
 import sys
+import os
+from tkinter import filedialog  # for Python 3
+from configparser import ConfigParser
 
 valid_extensions = (".png", ".jpg", ".jpeg", ".bmp")
 similarity = 0.8
+
+
+def get_DEFAULT_ini_path():
+    return os.path.join(set_app_path(), "appData")
+
+
+def get_save_file_ini_path():
+    return filedialog.asksaveasfilename(initialdir=get_DEFAULT_ini_path(), title="Save setup file", filetypes=[("Setup files", "*.ini")])
+
+
+def get_open_file_ini_path():
+    return filedialog.askopenfilename(initialdir=get_DEFAULT_ini_path(), title="Open setup file", filetypes=[("Setup files", "*.ini")])
+
+
+def setup_saving_to_ini(
+        setup_path,
+        checkedboxes,
+        target_path,
+        similarity
+):
+
+    config = ConfigParser()
+
+    config["MATCHING"] = {
+        "images path": target_path,
+    }
+
+    config["FILE TYPES"] = {
+        ".png": checkedboxes[0],
+        ".jpg/.jpeg": checkedboxes[1],
+        ".bmp": checkedboxes[2]
+    }
+
+    config["MINIMAL SIMILARITY"] = {
+        "value": similarity,
+    }
+
+    if setup_path:
+        with open(setup_path, "w") as configfile:
+            config.write(configfile)
+    else:
+        raise OSError("There is no save path")
+
+
+def read_config_file(file):
+    """return string"""
+
+    config = ConfigParser()
+
+    try:
+        with open(file) as f:
+            config.read_file(f)
+    except IOError as error:
+        raise IOError(error)
+
+    return config
+
+
+DEFAULTS_file_path = "_DEFAULT.ini"
+
+
+def get_images_folder_path(config):
+    return config.get("MATCHING", "images path")
+
+
+def get_similarity(config):
+    return config.get("MINIMAL SIMILARITY", "value")
+
+
+def get_checked_extensions(config):
+    return config.items("FILE TYPES")
+
+
+def create_DEFAULT_setup_file(setup_path):
+    valid_extensions = [[".png", 1], [".jpg/.jpeg", 0], [".bmp", 0]]
+    checkedboxes = list(map(lambda x: x[1], valid_extensions))
+    target_path = ""
+    similarity = 0.8
+
+    setup_saving_to_ini(
+        setup_path,
+        checkedboxes,
+        target_path,
+        similarity
+    )
 
 
 def set_app_path():
